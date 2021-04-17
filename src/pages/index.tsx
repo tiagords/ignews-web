4fdd299a8c89bac6c9a0181da3973ead92905519
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2021-04-17 11:46:41 -0300

    Add Page: Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home - ig.news</title>
       </Head>
+
+      <main className={styles.content}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React world</span>
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding..." />
+      </main>
     </>
   );
 }
