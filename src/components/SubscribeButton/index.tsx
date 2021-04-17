1438991f3da230204555fb464071faabc106f8d2
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2021-04-17 14:19:47 -0300

    Add Stripe API

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from "./styles.module.scss";
 
-export function SubscribeButton() {
+interface SubscribeButtonProps {
+  priceID: string;
+}
+
+export function SubscribeButton({ priceID }: SubscribeButtonProps) {
   return (
     <button className={styles.subscribeButton} type="button">
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceID: string;
+    amount: number;
+    metadata: {
+      description;
+    };
+  };
+}
+
+export default function Home({ product }: HomeProps) {
+  const description = product.metadata.description.replace(
+    "React",
+    "<span>React</span>"
+  );
+
   return (
     <>
       <Head>
@@ -13,14 +30,12 @@ export default function Home() {
       <main className={styles.content}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>
-            News about the <span>React world</span>
-          </h1>
+          <h1 dangerouslySetInnerHTML={{ __html: description }}></h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceID={product.priceID} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding..." />
@@ -28,3 +43,27 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IhGx5BmTb8nAUsTGEarlOvE", {
+    expand: ["product"],
+  });
+
+  const metadata = price.product;
+
+  const product = {
+    priceID: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+    metadata,
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
